26e4d32426507e99f37713704d3824aa739b3e70
Gopika30304 <gopiboomi55@gmail.com>
2023-04-26 11:39:48 +0530

    Initial commit

---
+++ helmet/src/main/java/com/helmet/demo/service/HelmetService.java
@@ -0,0 +1,35 @@
+package com.helmet.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+
+import com.helmet.demo.model.HelmetModel;
+import com.helmet.demo.repository.HelmetRepository;
+
+@Service
+
+public class HelmetService {
+	@Autowired
+	HelmetRepository met;
+	public List <HelmetModel> main(){
+		return met.findAll();
+	}
+	public HelmetModel sub(HelmetModel h) {
+		return met.save(h);
+	}
+	public HelmetModel dub(HelmetModel h) {
+		return met.save(h);
+	}
+	public void del(int cost)
+	{
+		met.deleteById(cost);
+	}
+	public List<HelmetModel>sortHelmetModel(String field){
+		return met.findAll(Sort.by(field));
+	}
+
+
+}
